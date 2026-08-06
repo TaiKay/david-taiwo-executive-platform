cc1d6b101ff00f56a95050cddd89f2b20c842578
TaiKay <141767176+TaiKay@users.noreply.github.com>
2026-08-06 15:28:08 +0100

    RC-1 - Production Excellence

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,7 @@
 import createMDX from '@next/mdx';
 
 const withMDX = createMDX({ extension: /\.mdx?$/ });
-export default withMDX({ pageExtensions: ['ts', 'tsx', 'md', 'mdx'] });
+export default withMDX({
+  pageExtensions: ['ts', 'tsx', 'md', 'mdx'],
+  images: { formats: ['image/avif', 'image/webp'], deviceSizes: [360, 640, 750, 1080, 1440, 1920], imageSizes: [48, 96, 160, 256] },
+});
